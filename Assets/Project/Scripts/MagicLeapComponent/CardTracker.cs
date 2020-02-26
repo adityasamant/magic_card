ecdcf2fb157e913b4701d0cf02921daaa74ceb0a
billcshi <billcshi@gmail.com>
2020-02-26 14:11:48 -0800

    Upload everything to github.

---
+++ Assets/Project/Scripts/MagicLeapComponent/CardTracker.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEngine.XR.MagicLeap
+{
+    /// <summary>
+    /// CardTracker should be disable in the editor and enable after game start.
+    /// Add Card Images and Names to the array.
+    /// When Card been tracked, it will change status in private variable.
+    /// Using GetStates and GetTrackingName to know which card has been used.
+    /// </summary>
+    [AddComponentMenu("MagicLeapCard/CardTracker")]
+    public class CardTracker : MonoBehaviour
+    {
+        #region Pulbic Variable
+
+        /// <summary>
+        /// Image that needs to be tracked.
+        /// Do not resize the image, the aspect ratio of the image provided here
+        /// and the printed image should be the same. Set the "Non Power of 2"
+        /// property of Texture2D to none.
+        /// </summary>
+        [Tooltip("Texture2D  of image that needs to be tracked. Do not change the aspect ratio of the image. Set the \"Non Power of 2\" property of Texture2D to \"none\".")]
+        public Texture2D[] CardImageList;
+
+        /// <summary>
+        /// Card Name
+        /// The Length Of the Array should be the same as @CardImageList
+        /// </summary>
+        public string[] CardNameList;
+
+        /// <summary>
+        /// Longer dimension of the printed image target in scene units.
+        /// If width is greater than height, it is the width, height otherwise.
+        /// </summary>
+        [Tooltip("Longer dimension of the printed image target in scene units. If width is greater than height, it is the width, height otherwise.")]
+        public float LongerDimensionInSceneUnits;
+
+        /// <summary>
+        /// The Text box in UI show how many card you are tracking.
+        /// </summary>
+        [Tooltip("The Text box in UI show how many card you are tracking.")]
+        public Text NumberOfCard;
+
+        /// <summary>
+        /// The Text box in UI show which card you are tracking.
+        /// </summary>
+        [Tooltip("The Text box in UI show which card you are tracking.")]
+        public Text NameOfCard;
+        #endregion
+
+        #region Private Variable
+        private MLImageTarget[] myCardTarget;
+        private MLImageTargetResult[] myCardResult;
+        #endregion
+
+        #region UnityFunction
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        void Awake()
+        {
+            if(CardImageList.Length!=CardNameList.Length)
+            {
+                Debug.LogErrorFormat("CardTracker failed because the Image List doesn't match Name List");
+                return;
+            }
+            else
+            {
+                myCardTarget = new MLImageTarget[CardImageList.Length];
+                myCardResult = new MLImageTargetResult[CardImageList.Length];
+            }
+
+            MLResult result = MLImageTracker.Start();
+            if (!result.IsOk)
+            {
+                Debug.LogErrorFormat("MLImageTrackerBehavior failed to start image tracker. Reason: {0}", result);
+                return;
+            }
+
+            for(int i=0;i<CardImageList.Length;i++)
+            {
+                myCardTarget[i] = MLImageTracker.AddTarget(CardNameList[i], CardImageList[i], LongerDimensionInSceneUnits, CardEvent, false);
+                if (myCardTarget[i] == null)
+                {
+                    Debug.LogErrorFormat("MLImageTrackerBehavior failed to add target {0} to the image tracker.", gameObject.name);
+                }
+                myCardResult[i].Status = MLImageTargetTrackingStatus.NotTracked;
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(NumberOfCard)
+            {
+                NumberOfCard.text = "Now you are tracking " + GetStates().ToString() + "Cards.";
+            }
+            if(NameOfCard)
+            {
+                NameOfCard.text = "The Card You Tracking is: " + GetTrackingName();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            for(int i=0;i<CardNameList.Length;i++)
+            {
+                MLImageTracker.RemoveTarget(CardNameList[i]);
+            }
+            MLImageTracker.Stop();
+        }
+        #endregion
+
+        #region EventHandlers
+        private void CardEvent(MLImageTarget imageTarget, MLImageTargetResult newResult)
+        {
+            int index = -1;
+            for(int i=0;i<myCardTarget.Length;i++)
+            {
+                if(imageTarget==myCardTarget[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if(index==-1)
+            {
+                Debug.LogErrorFormat("CardTracker failed. Reason: Find an Unknown Card.");
+                return;
+            }
+            if (newResult.Status != myCardResult[index].Status)
+            {
+                if (newResult.Status == MLImageTargetTrackingStatus.NotTracked)
+                    Debug.Log(("Card {0} Missed.", CardNameList[index]));
+                if (newResult.Status == MLImageTargetTrackingStatus.Tracked)
+                    Debug.Log(("Card {0} Find.", CardNameList[index]));
+                myCardResult[index] = newResult;
+            }
+        }
+        #endregion
+
+        #region Public Function
+        /// <summary>
+        /// Get number of Image been tracking
+        /// </summary>
+        /// <returns>
+        /// 0: No Card been Tracking
+        /// 1: 1 Card been Tracking, Well to go to next step
+        /// >1: More than 1 cards been tracking, Cannot go to next step
+        /// </returns>
+        public int GetStates()
+        {
+            int num = 0;
+            for(int i=0;i<myCardResult.Length;i++)
+            {
+                if(myCardResult[i].Status==MLImageTargetTrackingStatus.Tracked)
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// Get the name of Tracking Card, If more than one card been tracking return "MORE", If no card been tracking return "NON"
+        /// </summary>
+        /// <returns>
+        /// "NON", No card been tracking, cannot go to next step
+        /// "MORE", more than 1 card been tracking, cannot go to next step
+        /// "$CardName", return card name, can go to next step
+        /// "ERROR", Something Wrong!
+        /// </returns>
+        public string GetTrackingName()
+        {
+            int num = GetStates();
+            if(num==0)
+            {
+                return "NON";
+            }
+            else if(num>1)
+            {
+                return "MORE";
+            }
+            for(int i=0;i<myCardResult.Length;i++)
+            {
+                if(myCardResult[i].Status==MLImageTargetTrackingStatus.Tracked)
+                {
+                    return CardNameList[i];
+                }
+            }
+            return "ERROR";
+        }
+        #endregion
+    }
+}
---
+++ Assets/Project/Scripts/PlayerController/PlayerFSM.cs
@@ -0,0 +1,248 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
+
+namespace GameLogic
+{
+    /// <summary>
+    /// When this player is end its turn, it will pop a delegate to Main Logic
+    /// </summary>
+    /// <param name="id">
+    /// The playerid, should be unique for each Player
+    /// </param>
+    public delegate void PlayerEnd(int id);
+
+    /// <summary>
+    /// PlayerStates are states for the Finited State Machine
+    /// </summary>
+    public enum PlayerStates
+    {
+        Init,
+        WaitForStart,
+        Main_Phase,
+        Confirm_Phase,
+        Spawn_Phase,
+        End,
+        Error
+    };
+
+    /// <summary>
+    /// Using a Finited State Machine to control the game loop in Player Turn
+    /// </summary>
+    public class PlayerFSM : MonoBehaviour
+    {
+        #region Private Variable
+        /// <summary>
+        /// myState record the states of the player
+        /// </summary>
+        private PlayerStates myState=PlayerStates.Init;
+
+        /// <summary>
+        /// Only Init when game start
+        /// </summary>
+        private bool _bInit=false;
+
+        /// <summary>
+        /// Remember the startTime, wait 5 seconds for MagicLeap to StartUp
+        /// </summary>
+        private float startTime;
+
+        /// <summary>
+        /// The Card Player played
+        /// </summary>
+        private string PlayedCardName;
+
+        #endregion
+
+        #region Event Define
+        UnityEvent Event_PlayerTurnStart;
+        #endregion
+
+        #region Public Variable
+        /// <summary>
+        /// Start Hand Tracker when game Launch
+        /// </summary>
+        public GameObject myHandTrackerObject;
+        ///<summary>
+        /// A link to HandTracker
+        ///</summary>
+        public HandTracker myHandTracker;
+        /// <summary>
+        /// Start Card Tracker when game Launch
+        /// </summary>
+        public GameObject myCardeTrackerObject;
+        /// <summary>
+        /// A link to CardTracker
+        /// </summary>
+        public CardTracker myCardTracker;
+        /// <summary>
+        /// Player Identity, should be unique for each player
+        /// </summary>
+        public int PlayerId;
+
+        /// <summary>
+        /// Define the object for delegate
+        /// </summary>
+        public PlayerEnd PlayerEnd;
+
+        /// <summary>
+        /// Monster Prefab List
+        /// </summary>
+        public GameObject[] MonsterPrefabLists;
+
+        /// <summary>
+        /// The Text box in UI show Instructions based on states.
+        /// </summary>
+        [Tooltip("The Text box in UI show Instructions based on states.")]
+        public Text Instructions;
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(_bInit)
+            {
+                myState = PlayerStates.Init;
+                startTime = Time.time;
+                _bInit = true;
+            }
+            //Event Init
+            if(Event_PlayerTurnStart == null)
+            {
+                Event_PlayerTurnStart = new UnityEvent();
+            }
+            Event_PlayerTurnStart.AddListener(PlayerTurnStartInvoke);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch(myState)
+            {
+                case (PlayerStates.Init):
+                    Debug.Log("PlayerStates=Init");
+                    Instructions.text = "Now is Init States. Wait 5 seconds.";
+                    float nowTime = Time.time;
+                    if(nowTime-startTime>5.0f)
+                    {
+                        if(myHandTrackerObject)
+                            myHandTrackerObject.SetActive(true);
+                        if(myCardeTrackerObject)
+                            myCardeTrackerObject.SetActive(true);
+                        if(myHandTracker)
+                        {
+                            myHandTracker.FindPose += HandGesture;
+                        }
+                        myState = PlayerStates.WaitForStart;
+                    }
+                    break;
+                case (PlayerStates.WaitForStart): //Wait Event From Main Logic
+                    Debug.Log("PlayerStates=WaitForStart");
+                    Instructions.text = "Now is not your turn. Please wait for the game going.";
+                    nowTime = Time.time; //Test Only
+                    if (nowTime - startTime > 10.0f)
+                    {
+                        myState = PlayerStates.Main_Phase;
+                        myHandTracker.Event_enableOKtracked.Invoke();
+                    }
+                    break;
+                case (PlayerStates.Main_Phase): // Wait For Hand Event From Hand Tracker
+                    Debug.Log("PlayerStates=Main_Phase");
+                    Instructions.text = "Now is your turn, Main-Phase.\n Please choose a card. \n And shape your right hand to OK pose to confirm.";
+                    break;
+                case (PlayerStates.Confirm_Phase): // Wait For Hand (OpenHand or Fist) From Hand Tracker
+                    Debug.Log("PlayerStates=Confirm_Phase");
+                    Instructions.text = "Now is your turn, Confirm-Phase.\n Please use Open-Hand pose to comfirm. \n Or use Fist pose to go back.";
+                    break;
+                case (PlayerStates.Spawn_Phase):
+                    Debug.Log("PlayerStates=Spawn_Phase");
+                    //Spawn Actor Here
+                    Instructions.text = "Now is your turn, Spawn-Phase.\n Your monster is spawning into battlefield.";
+                    Debug.Log("Spawn A Charcter Name:" + PlayedCardName);
+                    if(PlayedCardName=="aaa")
+                    {
+                        GameObject monsterAAA = Instantiate(MonsterPrefabLists[0]);
+                    }
+                    else if(PlayedCardName=="bbb")
+                    {
+                        GameObject monsterBBB = Instantiate(MonsterPrefabLists[1]);
+                    }
+                    myState = PlayerStates.End;
+                    break;
+                case (PlayerStates.End):
+                    //Return the game control loop to main logic
+                    Debug.Log("PlayerStates=End");
+                    Instructions.text = "Now is your turn, End-Phase. Switch to next turn.";
+                    PlayerEnd(PlayerId);
+                    myState = PlayerStates.WaitForStart;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #region Event Handler
+        /// <summary>
+        /// Call this function when Event_PlayerTurnStart invoke
+        /// Change User state from WaitForStart to Main_Phase
+        /// </summary>
+        void PlayerTurnStartInvoke()
+        {
+             if(myState==PlayerStates.WaitForStart)
+            {
+                myState = PlayerStates.Main_Phase;
+                myHandTracker.Event_enableOKtracked.Invoke();
+            }
+        }
+        #endregion
+
+        #region Delegate Handler
+        /// <summary>
+        /// Invoke when receive delegate from HandTracker
+        /// </summary>
+        /// <param name="PoseId">
+        /// Id=0, OK Pose
+        /// Id=1, OpenHand Pose
+        /// id=2, Fist Pose
+        /// </param>
+        private void HandGesture(int PoseId)
+        {
+            Debug.Log("HandGestrue here!");
+            if(PoseId==0)
+            {
+                if(myState==PlayerStates.Main_Phase)
+                {
+                    if(myCardTracker.GetStates()==1)
+                    {
+                        PlayedCardName = myCardTracker.GetTrackingName();
+                        myHandTracker.Event_enableFistTracked.Invoke();
+                        myHandTracker.Event_enableOpenHandtracked.Invoke();
+                        myState = PlayerStates.Confirm_Phase;
+                    }
+                }
+            }
+            if(PoseId==1)
+            {
+                if(myState==PlayerStates.Confirm_Phase)
+                {
+                    myHandTracker.Event_disableAllTracked.Invoke();
+                    myState = PlayerStates.Spawn_Phase;
+                }
+            }
+            if(PoseId==2)
+            {
+                if(myState==PlayerStates.Confirm_Phase)
+                {
+                    PlayedCardName = "NON";
+                    myState = PlayerStates.Main_Phase;
+                }
+            }
+        }
+        #endregion
+    }
+}
+
+

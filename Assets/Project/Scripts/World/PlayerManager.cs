2bb07ad2c41ce10bff68d92286f4cd4f762a78bf
formyfamily <zkuang@usc.edu>
2020-02-27 02:08:16 -0800

    added world, monster, player manager, still working on it

---
+++ Assets/Project/Scripts/World/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Monsters;
+
+public class PlayerManager : MonoBehaviour
+{
+    /// <summary>
+    /// All monsters of this player on board.
+    /// </summary>
+    public HashSet<int> monsterIds;
+    public World world;
+    public bool isServer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterIds.Clear();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

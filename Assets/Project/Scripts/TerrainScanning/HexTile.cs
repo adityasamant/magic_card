46ec4fe6ae75820b00385867edfea263f83e5fe9
Luwei Ge <luweige@usc.edu>
2020-03-08 01:09:16 -0800

    Replace Dijikstra With A*

--- Assets/Project/Scripts/TerrainScanning/HexTile.cs
+++ Assets/Project/Scripts/TerrainScanning/HexTile.cs
@@ -39,5 +39,24 @@ namespace TerrainScanning
         {
             return this.id;
         }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            HexTile tile = obj as HexTile;
+            return this.x == tile.getX() && this.y == tile.getY();
+        }
+        
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            throw new System.NotImplementedException();
+            return base.GetHashCode();
+        }
     }
 }

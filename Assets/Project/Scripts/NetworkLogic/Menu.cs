3a7dd711cb60a2bd6bf20ec64fa0f58301056478
billcshi <billcshi@gmail.com>
2020-04-02 01:31:08 -0700

    Add Photon and test scene to ML

---
+++ Assets/Project/Scripts/NetworkLogic/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UdpKit;
+using UnityEngine;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        base.BoltStartDone();
+        if(BoltNetwork.IsServer)
+        {
+            string matchName = "Test Match";
+            BoltNetwork.SetServerInfo(matchName, null);
+            BoltNetwork.LoadScene("TestMap");
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        base.SessionListUpdated(sessionList);
+        foreach(var itr in sessionList)
+        {
+            UdpSession udpSession = itr.Value as UdpSession;
+
+            if(udpSession.Source==UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(udpSession);
+            }
+        }
+    }
+}
---
+++ Assets/Project/Scripts/NetworkLogic/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Bolt.EntityBehaviour<ICubeState>
+{
+    //void Start()
+    public override void Attached()
+    {
+        base.Attached();
+        state.SetTransforms(state.CubeTransform, transform);
+    }
+
+    // void Update()
+    public override void SimulateOwner()
+    {
+        base.SimulateOwner();
+        var speed = 4f;
+        var movement = new Vector3(Random.value - 0.5f, Random.value - 0.5f, Random.value - 0.5f);
+        if(movement!=Vector3.zero)
+        {
+            transform.position += movement * speed * BoltNetwork.FrameDeltaTime;
+        }
+    }
+}
